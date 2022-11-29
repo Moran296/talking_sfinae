dbf41121fcc42959aba689c20fb4cf9813f77316
Moran <moriki@gmail.com>
2022-11-29 08:36:23 +0200

    Remove std::foraward and rvalue references for simplicity
    Signed-off-by: Moran <moriki@gmail.com>

--- four_and_a_half.cpp
+++ four_and_a_half.cpp
@@ -9,10 +9,12 @@
 /*
     Benchmark gets any callable and parameters.
     It runs the function, tells us how much time it took and returns the result of the function.
+
+    //***NOTE rvalue references and std::forward are not used for simplicity.
 */
 
 template <typename T, typename... Args>
-auto Benchmark(const char* name, T&& func, Args&&... args) -> decltype(func(args...)) {
+auto Benchmark(const char* name, T func, Args... args) -> decltype(func(args...)) {
     auto start = std::chrono::system_clock::now();
     auto result = func(args...);
     auto end = std::chrono::system_clock::now();
@@ -74,12 +76,14 @@ The difference should be mainly in the function declaration, we use return type 
 1. For the first function, enable if the return type is void, return void.
 
 template <typename T, typename... Args>
-auto Benchmark(const char* name, T&& func, Args&&... args) -> typename std::enable_if<std::is_void<decltype(func(args...))>::value, void>::type {
+auto Benchmark(const char* name, T func, Args... args) -> typename std::enable_if<std::is_void<decltype(func(args...))>::value, void>::type {
 
 2. For the second function, enable if the return type is not void, return the return type of the invocation
 
 template <typename T, typename... Args>
-auto Benchmark(const char* name, T&& func, Args&&... args) -> typename std::enable_if<!std::is_void<decltype(func(args...))>::value, decltype(func(args...))>::type {
+auto Benchmark(const char* name, T func, Args... args) -> typename std::enable_if<!std::is_void<decltype(func(args...))>::value, decltype(func(args...))>::type {
+
+
 
 */
 
